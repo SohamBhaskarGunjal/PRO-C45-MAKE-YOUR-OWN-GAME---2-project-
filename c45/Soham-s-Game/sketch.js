fb72e883972626b37a7056620dc01b1019914559
SohamBhaskarGunjal <76810268+SohamBhaskarGunjal@users.noreply.github.com>
2021-09-23 19:54:11 +0530

    PRO-C45: MAKE YOUR OWN GAME - 2
    PRO-C45: MAKE YOUR OWN GAME - 2 project

---
+++ c45/Soham-s-Game/sketch.js
@@ -0,0 +1,13 @@
+var bg,bgImg;
+var player,shooterImg,shooter_shooting;
+
+function preload(){
+ bgImg=loadImage("assets/bg.jpeg");
+ shooterImg=loadImage("assets/shooter_2.png");
+ shooter_shooting=loadImage("assets/shooter_3.png");
+ 
+}
+
+function setup(){
+
+}
